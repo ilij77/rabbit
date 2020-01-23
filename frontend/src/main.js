41f838906a2b7dc74bb39b4c7d52686364cff853
ilij <ilij@ya.ru>
2020-01-24 01:45:58 +0300

    Установка бутстрапа и пакета иконок

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+axios.defaults.baseURL=process.env.VUE_APP_API_URL;
+Vue.use(BootstrapVue);
 new Vue({
   router,
   store,
