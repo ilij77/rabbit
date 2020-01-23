331e668943604c565bcde2c599fdff0cb75ff0eb
ilij <ilij@ya.ru>
2020-01-23 03:25:40 +0300

    Перенос начальной верстки

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1>Welcome to App</h1>
+      <p class="mb-0">
+        Congratulations! You have successfully created new application.
+      </p>
+      <ApiStatus/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ApiStatus from '@/components/ApiStatus.vue'
+
+export default {
+  name: 'home',
+  components: {
+    ApiStatus
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .jumbotron{
+    padding: 2rem;
+    p:last-of-type{
+      margin-bottom: 0;
+    }
+  }
+</style>
