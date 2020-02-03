40951e91ca7d72520421becb6c1140c34bde8f7f
ilij <ilij@ya.ru>
2020-02-03 03:22:41 +0300

    Установка DoctrineMigrations

--- api/config/container.php
+++ api/config/container.php
@@ -1,6 +1,13 @@
 <?php
 declare(strict_types=1);
-use Slim\Container;
+use Zend\ConfigAggregator\ConfigAggregator;
+use Zend\ConfigAggregator\PhpFileProvider;
 
-$config = require 'config/config.php';
-return new Container($config);
+$aggregator = new ConfigAggregator([
+	new PhpFileProvider(__DIR__ . '/common/*.php'),
+	new PhpFileProvider(__DIR__ . '/' . (getenv('API_ENV') ?: 'prod') . '/*.php'),
+]);
+
+$config = $aggregator->getMergedConfig();
+
+return new \Slim\Container($config);
